c05f94af86b08e35fe5e21ada3de43ed2507bc47
Vali <olaruvali25@gmail.com>
2026-03-03 17:46:08 +0200

    Update homepage copy and default locale

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["ro", "en"],
   defaultLocale: "ro",
+  localeDetection: false,
   localePrefix: "as-needed"
 });
 
